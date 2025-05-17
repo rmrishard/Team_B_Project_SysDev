8bfe7f9d8ff705a6cd2fe9706ac4d2ddf0741ffa
kaurinz <906530+kaurinz@users.noreply.github.com>
2025-05-17 11:20:30 -0400

    update fastapi base files with example routers

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,14 @@
 from typing import Union
-
 from fastapi import FastAPI
+
+from .routers import example
 
 app = FastAPI()
 
+# Include the example.router
+# All resources should be built in their own routers file, following that of "example"
+app.include_router(example.router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
