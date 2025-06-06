ee5171a800f0a47c0e217d560ece1cd2aa0d6483
kaurinz <906530+kaurinz@users.noreply.github.com>
2025-06-05 21:03:27 -0400

    BACKEND: Fix product_review model field aliasing; update product_reviews to utilize helper functions from utils.orm

--- backend/app/models/product_reviews.py
+++ backend/app/models/product_reviews.py
@@ -1,29 +1,35 @@
 import datetime
 import uuid
 
+from typing import Optional
 from pydantic import ConfigDict
 from sqlmodel import Field, SQLModel
 
 class ProductReviewBase(SQLModel):
     __tablename__ = "product_review"
     model_config = ConfigDict(extra='ignore')
-    product_id_fk: uuid.UUID = Field(alias='product_id', default_factory=uuid.uuid4)
-    user_id_fk: uuid.UUID = Field(alias='user_id', default_factory=uuid.uuid4)
+    product_id_fk: uuid.UUID = Field(alias='product_id', default_factory=uuid.uuid4,schema_extra={'serialization_alias': 'product_id'})
+    user_id_fk: uuid.UUID = Field(alias='user_id', default_factory=uuid.uuid4, schema_extra={'serialization_alias': 'user_id'})
     rating: int
-    title: str
-    content: str
-    verified_buyer: bool
+    title: Optional[str]
+    content: Optional[str]
+    verified_buyer: bool = Field(default=False)
     creation_time: datetime.datetime = Field(default_factory=datetime.datetime.now)
 
 # When table=True validation is not done, thus it must be done manually
 class ProductReview(ProductReviewBase, table=True):
-    product_review_id_pk: uuid.UUID = Field(alias='id', primary_key=True, default_factory=uuid.uuid4, index=True)
+    product_review_id_pk: uuid.UUID = Field(alias='id', primary_key=True, default_factory=uuid.uuid4, index=True, schema_extra={'serialization_alias': 'id'})
+
+    # This allows for helper functions to retrieve the appropriate id field
+    @classmethod
+    def getIdField(cls):
+        return cls.product_review_id_pk
 
 class ProductReviewCreate(ProductReviewBase):
     pass
 
 class ProductReviewPublic(ProductReviewBase):
-    product_review_id_pk: uuid.UUID = Field(alias='id', primary_key=True, default_factory=uuid.uuid4, index=True)
+    product_review_id_pk: uuid.UUID = Field(alias='id', primary_key=True, default_factory=uuid.uuid4, index=True, schema_extra={'serialization_alias': 'id'})
 
 class ProductReviewUpdate(ProductReviewBase):
     pass
