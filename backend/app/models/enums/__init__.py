a396e33cc531e2f182cf0c643090b547338457db
kaurinz <906530+kaurinz@users.noreply.github.com>
2025-06-07 09:43:21 -0400

    BACKEND: add payment_type enum model

--- backend/app/models/enums/__init__.py
+++ backend/app/models/enums/__init__.py
@@ -1,4 +1,5 @@
 EnumsRegistered = dict()
 
-__all__ = ['order_line_item_types','order_types']
+# Update with new enum types as they are added
+__all__ = ['order_line_item_types','order_types','payment_types']
 
