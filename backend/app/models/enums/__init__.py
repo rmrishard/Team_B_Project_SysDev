882d19e368246cc4ab557f6917c7f264a5a2c36c
kaurinz <906530+kaurinz@users.noreply.github.com>
2025-06-07 10:12:26 -0400

    BACKEND: add order_status_type enum model

--- backend/app/models/enums/__init__.py
+++ backend/app/models/enums/__init__.py
@@ -1,5 +1,8 @@
 EnumsRegistered = dict()
 
 # Update with new enum types as they are added
-__all__ = ['order_line_item_types','order_types','payment_types']
+__all__ = ['order_line_item_types',
+           'order_types',
+           'payment_types',
+           'order_status_types']
 
