ff5d62812bfd34be49c728d33e8f20a300c06ab9
rmrishard <107164504+rmrishard@users.noreply.github.com>
2025-06-12 13:05:22 -0400

    BACKEND: Initial creation of order_line_Item

---
+++ backend/app/routers/mocking/order_line_items.py
@@ -0,0 +1,24 @@
+fake_order_line_items = {
+    "order_line_items": [
+        {
+            "id": "11111111-1111-1111-1111-111111111111",
+            "product_id": "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+            "product_name": "Goat Cheese",
+            "product_qty": 2,
+            "product_price": 10.50,
+            "stock_quantity": 50,
+            "order_id": "a954bdb3-08fb-409c-9c7a-b90fcd127025",
+            "line_type_id": 1
+        },
+        {
+            "id": "22222222-2222-2222-2222-222222222222",
+            "product_id": "cccccccc-cccc-cccc-cccc-cccccccccccc",
+            "product_name": "Olive Olive",
+            "product_qty": 1,
+            "product_price": 20.00,
+            "stock_quantity": 30,
+            "order_id": "fbe3335b-5071-43ca-884d-a24c3bce338e",
+            "line_type_id": 1
+        }
+    ]
+}
