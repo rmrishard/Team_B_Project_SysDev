4808e5e1f5c1a6ff0cd41052f74499adf2f6e13e
rmrishard <107164504+rmrishard@users.noreply.github.com>
2025-06-11 22:24:45 -0400

    Backend : Initial creation of orders

---
+++ backend/app/routers/mocking/orders.py
@@ -0,0 +1,49 @@
+fake_orders = {
+  "orders":
+  [
+    {
+      "user_id": "a1b2c3d4-1111-2222-3333-abcdef123456",
+      "order_status_type_id": 1,
+      "order_type_id": 2,
+      "payment_type_id": 1,
+      "shipping_address": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "billing_address": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "special_request": "Please call on arrival",
+      "total": 45.50,
+      "creation_time": "2025-06-10T10:30:00Z"
+    },
+    {
+      "user_id": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "order_status_type_id": 2,
+      "order_type_id": 1,
+      "payment_type_id": 2,
+      "shipping_address": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "billing_address": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "special_request": None,
+      "total": 120.00,
+      "creation_time": "2025-06-09T14:00:00Z"
+    },
+    {
+      "user_id": "c3d4e5f6-3333-4444-5555-cdefab345678",
+      "order_status_type_id": 1,
+      "order_type_id": 1,
+      "payment_type_id": 1,
+      "shipping_address": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "billing_address": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "special_request": "Leave package at back door",
+      "total": 89.99,
+      "creation_time": "2025-06-08T09:15:00Z"
+    },
+    {
+      "user_id": "d4e5f6g7-4444-5555-6666-defabc456789",
+      "order_status_type_id": 3,
+      "order_type_id": 2,
+      "payment_type_id": 3,
+      "shipping_address": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "billing_address": "b2c3d4e5-2222-3333-4444-bcdefa234567",
+      "special_request": "Ring twice",
+      "total": 150.25,
+      "creation_time": "2025-06-07T17:45:00Z"
+    }
+  ]
+}
