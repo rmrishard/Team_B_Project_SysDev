e5d22cc06fbcdac1745f7e76be98ca9e2c7edf72
kaurinz <906530+kaurinz@users.noreply.github.com>
2025-06-17 17:17:45 -0400

    BACKEND: Relax constraint on foreign key requirement for user_id, enforce uniqueness constraints in DB in shopping cart table on both user_id and guest_token (to enforce single 'instace' or a cart per user); implement related model changes to shopping_cart and modify validation/model-based constraints

--- backend/app/models/shopping_carts.py
+++ backend/app/models/shopping_carts.py
@@ -1,16 +1,24 @@
 import uuid
 import datetime
-from pydantic import ConfigDict, AliasChoices
+from pydantic import ConfigDict, AliasChoices, model_validator
 from sqlmodel import Field, SQLModel
-from typing import Optional
+from typing import Optional, Self
 
 class ShoppingCartBase(SQLModel):
     __tablename__ = "shopping_cart"
     model_config = ConfigDict(extra='ignore')
 
-    user_id_fk: uuid.UUID = Field(alias='user_id', schema_extra={'serialization_alias': 'user_id'})
+    #user_id may be null if user is a guest
+    user_id_fk: Optional[uuid.UUID] = Field(default=None, alias='user_id', schema_extra={'serialization_alias': 'user_id'})
     modification_time: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
     empty: bool = Field(default=True)
+    guest_token: Optional[uuid.UUID] = None
+
+    @model_validator(mode='after')
+    def check_unique_owner(self) -> Self:
+        if not self.guest_token and not self.user_id_fk:
+            raise ValueError('No valid ownership information associated with cart.')
+        return self
 
 class ShoppingCart(ShoppingCartBase, table=True):
     shopping_cart_id_pk: uuid.UUID = Field(alias='id', primary_key=True, default_factory=uuid.uuid4, index=True,
@@ -21,8 +29,9 @@ class ShoppingCart(ShoppingCartBase, table=True):
         return cls.shopping_cart_id_pk
 
 class ShoppingCartCreate(ShoppingCartBase):
-    user_id_fk: uuid.UUID = Field(schema_extra={'serialization_alias': 'user_id',
+    user_id_fk: Optional[uuid.UUID] = Field(default=None, schema_extra={'serialization_alias': 'user_id',
                                                'validation_alias': AliasChoices('user_id')})
+    guest_token: Optional[uuid.UUID] = None
 
 class ShoppingCartPublic(ShoppingCartBase):
     shopping_cart_id_pk: uuid.UUID = Field(alias='id', primary_key=True, default_factory=uuid.uuid4, index=True,
@@ -34,3 +43,4 @@ class ShoppingCartUpdate(ShoppingCartBase):
     user_id_fk: None = Field(default=None, alias='user_id')
     modification_time: Optional[datetime.datetime] = None
     empty: Optional[bool] = None
+    guest_token: Optional[uuid.UUID] = None
--- backend/app/routers/shopping_carts.py
+++ backend/app/routers/shopping_carts.py
@@ -2,6 +2,8 @@ from typing import List
 import uuid
 from fastapi import APIRouter, HTTPException
 from sqlmodel import Session
+from sqlalchemy.exc import IntegrityError
+
 from app import engine
 from .mocking.shopping_carts import fake_shopping_carts as mock_items
 from ..models.shopping_carts import *
@@ -37,9 +39,14 @@ def update_item(item_id: uuid.UUID, item: ShoppingCartUpdate):
 
 @router.post("/upload/", response_model=List[ShoppingCartPublic])
 def create_items(shopping_carts: List[ShoppingCartCreate]):
-    with Session(engine) as session:
-        for item in shopping_carts:
-            valid_item = ShoppingCart.model_validate(item)
-            session.add(valid_item)
-        session.commit()
+    try:
+        with Session(engine) as session:
+            for item in shopping_carts:
+                valid_item = ShoppingCart.model_validate(item)
+                session.add(valid_item)
+            session.commit()
+    except IntegrityError as error:
+        print(error)
+        raise HTTPException(status_code=409, detail="Error creating cart")
+
     return shopping_carts
