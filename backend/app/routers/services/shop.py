50121ff1e08f5fdb515e0b6c55c4ad763c96a9d6
kaurinz <906530+kaurinz@users.noreply.github.com>
2025-06-18 22:41:35 -0400

    BACKEND: WIP, initial commit of functioning operation to get the cart with its associated cart_items and related products

--- backend/app/routers/services/shop.py
+++ backend/app/routers/services/shop.py
@@ -1,13 +1,17 @@
 from typing import List, Any
 import uuid
 from fastapi import APIRouter, HTTPException, Request
-from sqlmodel import Session
+from sqlmodel import Session, select, col
 from sqlalchemy.exc import IntegrityError
 
 from app import engine
 from app.models.shopping_carts import *
 from app.utils.orm import ReadItems, UpdateItems
 from app.models.services.carts import CartPublic
+from .authorize import confirm_session
+from ...models.shopping_cart_items import ShoppingCartItem
+from ...models.shopping_carts import ShoppingCart
+from ...models.products import Product
 
 router = APIRouter(
     prefix="/shop",
@@ -15,13 +19,121 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+GUEST_CART_TIMEOUT = 14     # TODO: Number of days to allow a GUEST cart to be active before expiring (set 0 for no limit)
+USER_CART_TIMEOUT = 14      # TODO: Number of days to allow a USER cart to be active before expiring (set 0 for no limit)
+
 #Utilize POST for now, but information should be pulled directly from the session
 @router.get("/cart/", response_model=Any)
-async def retrieve_cart(request: Request):
-    request.session["cart"] = {"items": ["Apple","Banana","Carrots"]}
+async def get_cart(request: Request):
+    shopping_cart_id = None
+    products = None
 
-    # items = ReadItems.read(ShoppingCart)
-    # if not items:
-    #     raise HTTPException(status_code=404, detail="No shopping carts found")
-    # return items
-    return
+    # Retrieve the Cart
+    cart = retrieve_cart(request)
+    if cart:
+        # Store cart_id in encrypted session to expedite future processing
+        shopping_cart_id = str(cart.shopping_cart_id_pk)
+        request.session["shopping_cart_id"] = shopping_cart_id
+    else:
+        raise HTTPException(status_code=404, detail="Failed to retrieve a cart.")
+
+    # Retrieve shopping_cart_items
+    cart_items = retrieve_cart_items(shopping_cart_id)
+
+    cart_items = None if not cart_items else cart_items #For consistency set to None (not empty list)
+
+    if cart_items:
+        product_ids = map(lambda item: item.product_id_fk, cart_items)
+        products = retrieve_cart_products(product_ids)
+
+
+    return {"cart":cart,"cart_items":cart_items,"cart_products":products}
+
+def retrieve_cart(request: Request):
+    active_session = confirm_session(request.session)
+    cart = None
+
+    if active_session:
+
+        # Attempt to retrieve cart_id from session if present
+        cart_id = request.session.get("shopping_cart_id", None)
+        cart = ReadItems.with_id(ShoppingCart, cart_id) if cart_id else None
+
+        if cart:
+            return cart
+
+        # Fallback to other methods for retrieving shopping cart
+        user_id = request.session["credential"].get("user_id", None)
+        guest_token = request.session["credential"].get("guest_token", None)
+
+        # Pass both, priority given to user_id
+        cart = retrieve_cart_from_db(user_id=user_id,guest_token=guest_token)
+
+        if not cart:
+            # Pass both, priority given to user_id
+            cart = create_cart(user_id=user_id,guest_token=guest_token)
+
+        if not cart:
+            raise HTTPException(status_code=404, detail="Failed to retrieve a new cart")
+
+    else:
+        raise HTTPException(status_code=404, detail="Cannot find session. Are cookies disabled?")
+
+
+    return cart
+
+def retrieve_cart_from_db(user_id=None, guest_token=None):
+    cart = None
+
+    if not user_id and not guest_token:
+        return None
+
+    with Session(engine) as session:
+        statement = None
+
+        if user_id:
+            statement = select(ShoppingCart).where(ShoppingCart.user_id_fk == user_id)
+        elif guest_token:
+            statement = select(ShoppingCart).where(ShoppingCart.guest_token == guest_token)
+
+        if statement is not None:
+            cart = session.exec(statement).first()
+            # ENFORCE EXPIRATION
+
+    return cart
+
+def create_cart(user_id=None, guest_token=None):
+
+    if not user_id and not guest_token:
+        return None
+    else:
+        cart = None
+        with Session(engine) as session:
+            if user_id:
+                cart = ShoppingCart(user_id=user_id)
+            elif guest_token:
+                cart = ShoppingCart(guest_token=guest_token)
+
+            session.add(cart)
+            session.commit()
+            session.refresh(cart)
+
+    return cart
+
+def retrieve_cart_items(shopping_cart_id: str):
+    if not shopping_cart_id:
+        return None
+
+    by_cart_id = lambda sel: sel.where(ShoppingCartItem.shopping_cart_id_fk == shopping_cart_id)
+    cart_items = ReadItems.all_with(ShoppingCartItem, by_cart_id)
+
+    return cart_items
+
+def retrieve_cart_products(product_ids: List[str]):
+    if not product_ids:
+        return None
+
+    in_list = lambda sel: sel.where(col(Product.product_id_pk).in_(product_ids))
+    products = ReadItems.all_with(Product, in_list)
+
+    return products
